963e956a857222ddc61ed4c77601c96e6bae822f
livknapen <59344183+livknapen@users.noreply.github.com>
2024-03-27 14:39:04 +0100

    added arrow on home

--- includes/js/javascript.js
+++ includes/js/javascript.js
@@ -1,10 +1,17 @@
-/* Navbar */
-function openNav() {
-    document.getElementById("main-nav").style.height = "100%";
-    document.getElementById("burger").style.display = "none";
-}
+jQuery(document).ready(function() {
 
-function closeNav() {
-    document.getElementById("main-nav").style.height = "0%";
-    document.getElementById("burger").style.display = "revert";
-}
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+     
+    $(document).mousemove(function(e){
+      mouseX = e.pageX - 30;
+      mouseY = e.pageY - 30; 
+    });
+      
+    setInterval(function(){
+      xp += ((mouseX - xp)/6);
+      yp += ((mouseY - yp)/6);
+      $("#circle").css({left: xp +'px', top: yp +'px'});
+    }, 20);
+  
+  });
