80ef8d2cabbf93d0c43a5fe0846d9903bd04fb72
livknapen <59344183+livknapen@users.noreply.github.com>
2024-05-30 14:11:45 +0200

    added lo pages

--- includes/js/javascript.js
+++ includes/js/javascript.js
@@ -1,11 +1,5 @@
-$("button").click(function() {
+$(".arrow-button").click(function() {
     $('html,body').animate({
         scrollTop: $(".divmainall").offset().top},
-        'slow');
-});
-
-$("button").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".divLOall").offset().top},
         'slow');
 });
