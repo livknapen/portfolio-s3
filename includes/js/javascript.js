c330c5ddcddabbfc8fdaa980da8bcf3d0f09cf40
livknapen <59344183+livknapen@users.noreply.github.com>
2024-06-11 09:52:23 +0200

    added content to lo's and projects

--- includes/js/javascript.js
+++ includes/js/javascript.js
@@ -4,3 +4,38 @@ $(".arrow-button").click(function() {
         scrollTop: $(".divmainall").offset().top},
         'slow');
 });
+
+// Open PDF with button
+function openPDF1() {
+    const pdfPath = '/includes/media/Portfolio Liv.pdf#page=5';
+    // Open the PDF in a new tab
+    window.open(pdfPath, '_blank');
+}
+
+// Open PDF with button
+function openPDF2() {
+    const pdfPath = '/includes/media/Portfolio Liv.pdf#page=36';
+    // Open the PDF in a new tab
+    window.open(pdfPath, '_blank');
+}
+
+// Open PDF with button
+function openPDF3() {
+    const pdfPath = '/includes/media/Portfolio Liv.pdf#page=48';
+    // Open the PDF in a new tab
+    window.open(pdfPath, '_blank');
+}
+
+// Open PDF with button
+function openPDF4() {
+    const pdfPath = '/includes/media/Portfolio Liv.pdf#page=60';
+    // Open the PDF in a new tab
+    window.open(pdfPath, '_blank');
+}
+
+// Open PDF with button
+function openPDF5() {
+    const pdfPath = '/includes/media/Portfolio Liv.pdf#page=94';
+    // Open the PDF in a new tab
+    window.open(pdfPath, '_blank');
+}
