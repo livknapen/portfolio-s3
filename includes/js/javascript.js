af6009c391e8efa6ad27a1c9908e54d2e5b22075
livknapen <59344183+livknapen@users.noreply.github.com>
2024-06-28 12:21:32 +0200

    good portfolio pdf, and links

--- includes/js/javascript.js
+++ includes/js/javascript.js
@@ -7,35 +7,35 @@ $(".arrow-button").click(function() {
 
 // Open PDF with button
 function openPDF1() {
-    const pdfPath = './includes/media/PortfolioLiv.pdf#page=5';
+    const pdfPath = './includes/media/PortfolioLiv.pdf#page=4';
     // Open the PDF in a new tab
     window.open(pdfPath, '_blank');
 }
 
 // Open PDF with button
 function openPDF2() {
-    const pdfPath = './includes/media/PortfolioLiv.pdf#page=36';
+    const pdfPath = './includes/media/PortfolioLiv.pdf#page=23';
     // Open the PDF in a new tab
     window.open(pdfPath, '_blank');
 }
 
 // Open PDF with button
 function openPDF3() {
-    const pdfPath = './includes/media/PortfolioLiv.pdf#page=48';
+    const pdfPath = './includes/media/PortfolioLiv.pdf#page=29';
     // Open the PDF in a new tab
     window.open(pdfPath, '_blank');
 }
 
 // Open PDF with button
 function openPDF4() {
-    const pdfPath = './includes/media/PortfolioLiv.pdf#page=60';
+    const pdfPath = './includes/media/PortfolioLiv.pdf#page=37';
     // Open the PDF in a new tab
     window.open(pdfPath, '_blank');
 }
 
 // Open PDF with button
 function openPDF5() {
-    const pdfPath = './includes/media/PortfolioLiv.pdf#page=94';
+    const pdfPath = './includes/media/PortfolioLiv.pdf#page=63';
     // Open the PDF in a new tab
     window.open(pdfPath, '_blank');
 }
